f88efb032b72a9f5c3545fa9eb4c6b79dd00ce81
Martim <martimfj@al.insper.edu.br>
2019-04-13 02:15:27 -0300

    Lexer adaptado para roteiro 6

--- lexer.py
+++ lexer.py
@@ -18,19 +18,27 @@ class Tokenizer:
             self.position += 1
 
         if self.code[self.position] == "-":
-            self.actual = Token("UNARY_OP", "-")
+            self.actual = Token("ARITH_UNARY_OP", "-")
             self.position += 1
 
         elif self.code[self.position] == "+":
-            self.actual = Token("UNARY_OP", "+")
+            self.actual = Token("ARITH_UNARY_OP", "+")
             self.position += 1
 
         elif self.code[self.position] == "/":
-            self.actual = Token("BINARY_OP", "/")
+            self.actual = Token("ARITH_BINARY_OP", "/")
             self.position += 1
 
         elif self.code[self.position] == "*":
-            self.actual = Token("BINARY_OP", "*")
+            self.actual = Token("ARITH_BINARY_OP", "*")
+            self.position += 1
+
+        elif self.code[self.position] == ">":
+            self.actual = Token("COMP_BINARY_OP", ">")
+            self.position += 1
+
+        elif self.code[self.position] == "<":
+            self.actual = Token("COMP_BINARY_OP", "<")
             self.position += 1
 
         elif self.code[self.position] == "(":
@@ -55,18 +63,22 @@ class Tokenizer:
                 int_token += str(self.code[self.position])
                 self.position += 1
             self.actual = Token("INT", int(int_token))
-        
+
         elif self.code[self.position].isalpha():
             identifier_token = ""
             while self.position < len(self.code) and self.code[self.position].isalnum() or self.code[self.position] == "_":
                 identifier_token += str(self.code[self.position]).upper()
                 self.position += 1
 
-            reserved_words = ["PRINT", "BEGIN", "END"]
+            reserved_words = ["PRINT", "END", "WHILE", "WEND", "IF", "ELSE", "THEN", "INPUT"]
+            logical_operators = ["AND", "OR", "NOT"]
             if identifier_token in reserved_words:
                 self.actual = Token(identifier_token, identifier_token)
             else:
-                self.actual = Token("IDENTIFIER", identifier_token)
+                if identifier_token in logical_operators:
+                    self.actual = Token("LOGIC_BINARY_OP", identifier_token)
+                else:
+                    self.actual = Token("IDENTIFIER", identifier_token)
 
         else:
             raise ValueError("Tokenizer Error: Token {} is invalid".format(repr(self.code[self.position])))
